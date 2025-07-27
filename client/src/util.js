facbd28d33ea0731310dd0c1bb37351d88c50ab3
Shashi Hans <shashihans87@gmail.com>
2025-07-28 00:11:37 +0530

    server url added

--- client/src/util.js
+++ client/src/util.js
@@ -1,4 +1,4 @@
 const baseURL = (window.location.hostname === "localhost") ?
-          "http://localhost:4000" : " ";
+          "http://localhost:4000" : "https://edu-verse-4nzz.onrender.com";
 export const visitURL = `${baseURL}/api/visitor/visit`;
 export const countURL = `${baseURL}/api/visitor/count`;
